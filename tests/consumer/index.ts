906cd199027e5ece074e0a346a83d9a40eaff5b9
Priveetee <brahim.benyoucefpro@proton.me>
2026-08-22 17:35:17 +0200

    chore: validate TypeScript 7 package declarations

---
+++ tests/consumer/index.ts
@@ -0,0 +1,12 @@
+import {
+  type PlaybackWindowRequest,
+  type TypeTypeMseConfig,
+  TypeTypeMsePlayer,
+} from "@typetype/mse";
+
+const config = {} as TypeTypeMseConfig;
+const request = {} as PlaybackWindowRequest;
+
+void TypeTypeMsePlayer;
+void config;
+void request;
